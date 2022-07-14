872bcd64957b7387cc5ff64d25d5e4f22f7f70e6
VikaBels <vika.bel773@gmail.com>
2022-07-14 11:33:38 +0300

    fix problem

---
+++ app/src/main/java/com/example/lesson12/constants.kt
@@ -0,0 +1,10 @@
+package com.example.lesson12
+
+const val KEY_FOLDER_NAME = "/documents"
+const val TXT_NULL: String = "NULL"
+const val TXT_EMPTY: String = ""
+const val DEFAULT_NAME_FILE: String = "Document"
+const val SLASH_N: String = "\n"
+const val TAG_FOR_LIST: String = "tagListFragment"
+const val TAG_FOR_DETAIL: String = "tagDetailFragment"
+const val KEY_TRANSFER_NAME: String = "token"
